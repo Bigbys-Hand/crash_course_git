27d39c1c570796b30d8f819c695b9bd383c62287
Bigbys-Hand <lenomdeguerre75@gmail.com>
2019-11-09 18:39:46 -0800

    Initial commit

---
+++ crash_course_quickscripts.py
@@ -0,0 +1,13 @@
+message = "hello world!"
+print(message)
+message = "hello crash course!"
+print(message)
+# once I figure out github commits, I'll see this message
+
+
+def funtastic():
+    x = 20
+    print(x)
+
+
+funtastic()
