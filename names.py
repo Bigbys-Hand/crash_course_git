3bf95ea20b308d190405759bc0c28ebce6892194
Bigbys-Hand <lenomdeguerre75@gmail.com>
2019-11-09 22:42:28 -0800

    showing f-string and methods Now showing strips, newline and tab too.

--- names.py
+++ names.py
@@ -1,9 +1,9 @@
-first_name = "bartholomew"
+first_name = "bartholomew     "
 middle_name = "j."
 last_name = "scrubbs"
 suffix = "III"
-full_name = f"{first_name} {middle_name} {last_name}"
-greeting = f"Greetings, Lord {full_name.title()} the {suffix.upper()}!"
+full_name = f"{first_name.rstrip()} {middle_name} {last_name}"
+greeting = f"\tGreetings, Lord {full_name.title()} the {suffix.upper()}!\nWe wish you well on this, the day of your niece's sweet-16 nose job!"
 print(greeting)
 #putting the greeting inside a 'greeting' variable makes the final 'print' command much less clunky.
 
