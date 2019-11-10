e30e90f63049eede7f0e3e0a9c2e178959861808
Bigbys-Hand <lenomdeguerre75@gmail.com>
2019-11-10 13:37:09 -0800

    Showing lists and how to display them, plus altering their contents for the display you want.

--- lists.py
+++ lists.py
@@ -1,3 +1,11 @@
-bicycles = ['trek', 'cannondale', 'specialized', "whatever it was Eval Knievel's mom rode, idk", 'redline']
+bicycles = ['trek', 'cannondale', 'specialized', "whatever it was Eval Knievel's mom rode", 'redline']
 print(bicycles)
 print(bicycles[0].upper())
+# The first item in a list is always at 0, not at 1
+# You can run methods on list print commands! Fancy.
+print(bicycles[-1])
+# negative values let you select from right to left. There is no -0!
+print(bicycles[3])
+# 4th value is pulled
+boast = f"I am, in fact, partial to {bicycles[-1]} bicycles."
+print(boast)
